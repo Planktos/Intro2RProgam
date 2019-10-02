db3478f08068b71b0d3f5e8165b5f794636d1f47
Planktos <kellylynnrobinson@gmail.com>
2019-10-02 14:39:00 -0500

    inital commit

---
+++ exercise_summarize.R
@@ -0,0 +1,12 @@
+# Summarizing data
+# 2 Oct 2019
+# ------
+
+library(tidyverse)
+install.packages("nutshell")
+library(nutshell)
+
+#data we will be using today ----
+data("batting.2008")
+d <- batting.2008
+
